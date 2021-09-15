f5411333d008dcdcff14a3417766816b674cdd3b
leanwayne <lean_rap50@hotmail.com>
2021-09-15 15:35:18 -0300

    about component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import './App.css'
 import Cover from './components/cover/Cover'
 import NavBar from './components/navBar/NavBar'
+import About from './components/abaut/About'
 
 function App() {
     const [scrollHeight, setScrollHeight] = useState(0)
@@ -19,6 +20,7 @@ function App() {
         <div className="App">
             <NavBar isScrolling={scrollHeight}/>
             <Cover/>
+            <About/>
         </div>
     )
 }
